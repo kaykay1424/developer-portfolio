73f33ef59275006a06697bca444dc116a175c5b0
Kayla <kay_kay1424@yahoo.com>
2021-02-23 16:43:23 -0600

    edit intro on home page

--- js/main.js
+++ js/main.js
@@ -2,4 +2,25 @@ $(document).ready(function() {
     $('.nav__collapse-btn').click(function() {
         $(this).siblings('ul').fadeToggle();        
     })
+
+    // Home Page
+    if ($('body').prop('id') === 'home-page') {
+        function showLetter(letter,index) {
+            let numLetters = $('#profile__intro span').length;
+            if (index < numLetters) {
+                setTimeout(function() {
+                    letter.css('visibility','visible');
+                    index++;
+                    showLetter($('#profile__intro span').eq(index),index)
+                },100);                    
+            } else { // end of animation, show code link
+                setTimeout(function() {
+                    $('#profile__intro a').slideDown();
+                    $('.speech-bubble').removeClass('animate');
+                },1000)                    
+            }                
+        }
+        showLetter($('#profile__intro span').eq(0),0);
+        $('.speech-bubble').addClass('animate');
+    }
 })

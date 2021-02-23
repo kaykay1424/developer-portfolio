a9d0e4ca83326f2776090c95bf75ca2faf8a3f9c
Kayla <kay_kay1424@yahoo.com>
2021-02-23 17:58:44 -0600

    remove illustration, edit about/skills section on About page

--- js/main.js
+++ js/main.js
@@ -2,25 +2,74 @@ $(document).ready(function() {
     $('.nav__collapse-btn').click(function() {
         $(this).siblings('ul').fadeToggle();        
     })
-
-    // Home Page
-    if ($('body').prop('id') === 'home-page') {
-        function showLetter(letter,index) {
-            let numLetters = $('#profile__intro span').length;
-            if (index < numLetters) {
-                setTimeout(function() {
-                    letter.css('visibility','visible');
-                    index++;
-                    showLetter($('#profile__intro span').eq(index),index)
-                },100);                    
-            } else { // end of animation, show code link
-                setTimeout(function() {
-                    $('#profile__intro a').slideDown();
-                    $('.speech-bubble').removeClass('animate');
-                },1000)                    
-            }                
-        }
-        showLetter($('#profile__intro span').eq(0),0);
-        $('.speech-bubble').addClass('animate');
+    let page = $('body').prop('id');
+    switch(page) {
+        case 'home-page':
+            function showLetter(letter,index) {
+                let numLetters = $('#profile__intro span').length;
+                if (index < numLetters) {
+                    setTimeout(function() {
+                        letter.css('visibility','visible');
+                        index++;
+                        showLetter($('#profile__intro span').eq(index),index)
+                    },100);                    
+                } else { // end of animation, show code link
+                    setTimeout(function() {
+                        $('#profile__intro a').slideDown();
+                        $('.speech-bubble').removeClass('animate');
+                    },1000)                    
+                }                
+            }
+            showLetter($('#profile__intro span').eq(0),0);
+            $('.speech-bubble').addClass('animate');
+        break;
+        case 'about-page':
+            $('#skills__form').submit(function(e) {
+                e.preventDefault();            
+                let filtersArr = [];
+                let skillsTypeArr = [];
+                let develomentTypeArr = [];
+                $(this).find('[type="checkbox"]:checked').each(function() {
+                    let val = $(this).val();
+                    filtersArr.push(val);
+                    if (val === 'frontend' || val === 'backend' || val === 'fullstack') {
+                        develomentTypeArr.push(val);
+                    } else {
+                        skillsTypeArr.push(val);
+                    }
+                });
+                $('#skills__table tbody tr').removeClass('filtered');
+                filtersArr.map((filter) => {
+                    if (skillsTypeArr.length === 0) {
+                        if (develomentTypeArr.includes('frontend')) {
+                            $('[data-development-type="frontend"],[data-development-type="frontend"]').addClass('filtered');
+                        }
+                        if (develomentTypeArr.includes('backend')) {
+                            $('[data-development-type="backend"],[data-development-type="backend"]').addClass('filtered');
+                        }  
+                        if (develomentTypeArr.includes('fullstack')) {
+                            $('[data-development-type="fullstack"],[data-development-type="fullstack"]').addClass('filtered');
+                        } 
+                    } else if (develomentTypeArr.includes('frontend') || develomentTypeArr.includes('backend') || develomentTypeArr.includes('fullstack')) {
+                        if (develomentTypeArr.includes('frontend')) {
+                            $('[data-skill-type="'+filter+'"][data-development-type="frontend"],[data-skill-type="'+filter+'"][data-development-type="frontend"]').addClass('filtered');
+                        }
+                        if (develomentTypeArr.includes('backend')) {
+                            $('[data-skill-type="'+filter+'"][data-development-type="backend"],[data-skill-type="'+filter+'"][data-development-type="backend"]').addClass('filtered');
+                        }  
+                        if (develomentTypeArr.includes('fullstack')) {
+                            $('[data-skill-type="'+filter+'"][data-development-type="fullstack"],[data-skill-type="'+filter+'"][data-development-type="fullstack"]').addClass('filtered');
+                        }                  
+                    } 
+                    else {
+                        $('[data-skill-type="'+filter+'"]').addClass('filtered');
+                    }
+                })
+            });
+            $('#clear-filters').click(() => {
+                $('#skills__form [type="checkbox"]').prop('checked',false);
+                $('#skills__table tbody tr').removeClass('filtered');
+            });
+        break;
     }
 })
